4d753321c3be4b83a14323e65b9b60527c7b92cf
Tushant <programmertushant@gmail.com>
2018-08-27 11:26:36 +0545

    Initial Commit

---
+++ navi-macos/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  navi-macos
+//
+//  Created by Tushant Khatiwada on 8/27/18.
+//  Copyright © 2018 Tushant Khatiwada. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ navi-macos/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  navi-macos
+//
+//  Created by Tushant Khatiwada on 8/27/18.
+//  Copyright © 2018 Tushant Khatiwada. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
