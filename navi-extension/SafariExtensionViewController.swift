354888630cd335f7076e6650a44d1087e6ebea4f
Tushant <programmertushant@gmail.com>
2018-08-27 15:16:07 +0545

    integrate api to login

--- navi-extension/SafariExtensionViewController.swift
+++ navi-extension/SafariExtensionViewController.swift
@@ -10,14 +10,57 @@ import SafariServices
 
 class SafariExtensionViewController: SFSafariExtensionViewController {
     
+    
+    @IBOutlet weak var message: NSTextField!
     @IBOutlet weak var email: NSTextField!
     @IBOutlet weak var password: NSSecureTextField!
     static let shared = SafariExtensionViewController()
     override func viewDidLoad() {
-        self.preferredContentSize = NSSize(width: 300, height: 300)
+        self.preferredContentSize = NSSize(width: 300, height: 250)
+        message.stringValue = "email value is"
     }
     
+    
     @IBAction func userLogin(_ sender: Any) {
-        print("email----> \(email.stringValue) password ----> \(password.stringValue)")
+        print("email ----> \(email.stringValue)")
+        print("password ---> \(password.stringValue)")
+        let parameters = ["email": email.stringValue, "password": password.stringValue]
+        message.stringValue = "email is \(email.stringValue) \n pass is \(password.stringValue)"
+        let URI = URL(string: "https://api.production.navihq.com/auth/sign_in")
+        let session = URLSession.shared
+        var request = URLRequest(url: URI!)
+        request.httpMethod = "POST"
+        do {
+            request.httpBody = try JSONSerialization.data(withJSONObject: parameters, options: .prettyPrinted)
+        } catch let error{
+            print(error.localizedDescription)
+        }
+        request.addValue("application/json", forHTTPHeaderField: "Content-Type")
+        request.addValue("application/json", forHTTPHeaderField: "Accept")
+        print("request \(request)")
+        //create dataTask using the session object to send data to the server
+        let task = session.dataTask(with: request as URLRequest, completionHandler: { data, response, error in
+            
+            guard error == nil else {
+                return
+            }
+            
+            guard let data = data else {
+                return
+            }
+            
+            do {
+                //create json object from data
+                if let json = try JSONSerialization.jsonObject(with: data, options: .mutableContainers) as? [String: Any] {
+                    print("############################")
+                    print(json)
+                    // handle json...
+                }
+            } catch let error {
+                print("*************************")
+                print(error.localizedDescription)
+            }
+        })
+        task.resume()
     }
 }
